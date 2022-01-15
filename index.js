af5a1064aa30dde7d6ee78862cd87cf18b05e943
James Broucek <jamesbroucek@Jamess-iMac-2.local>
2022-01-15 07:12:08 -0800

    homepage

--- index.js
+++ index.js
@@ -6,34 +6,27 @@ const express = require('express');
 //Initializing the express app.
 const app = express();
 
-//Making the homepage route
-app.get('/',(req,res) => {
-    console.log('We did it!')
-    res.send('HOMEPAGE')
-});
+//Defining the view engine we'll be using
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'));
 
+//Making the homepage route
+app.get('/',(req,res) => {
+    console.log('We did it!')
+    res.render('HOME')
+});
+
+
 //Opening a wildcard route
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render(404).send('404 Page')
 });
 
 //Opening the server to listen for connections
 app.listen(process.env.PORT, function(){
     console.log('This port works')
 });
-
-/*    
-let http = require('http')
-let server = http.createServer(function(req, res) {
-    res.write('Welcome to Rest-Rant!')
-    res.end()
-})
-
-server.listen(3000, function(){
-console.log('This port works')
-})
-*/
 
 
